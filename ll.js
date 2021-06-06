4e371a6de98c2709168b35fd775d028522ac2405
amankumarmishra17 <85437233+amankumarmishra17@users.noreply.github.com>
2021-06-06 16:46:29 +0530

    Update ll.js

--- ll.js
+++ ll.js
@@ -2,7 +2,7 @@ function validate()
 {
     var username=document.getElementById('username').value;
     var password=document.getElementById('password').value;
-    if ( username=="Samir Kumar"&& password =="Kumar@123"||username =='Dilip Kumar Mishra' && password=="Kumar@123"||username=="Pawan Kumar" && password=="Kumar@123")
+    if ( username=="Samir Kumar"&& password =="Kumar@123"||username =='Dilip Kumar' && password=="Kumar@123"||username=="Pawan Kumar" && password=="Kumar@123")
 {
         window.open('https://amankumarmishra17.github.io/MISHRA/');
         return false;
